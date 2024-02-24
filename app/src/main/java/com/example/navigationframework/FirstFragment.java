53b8ce0bd67ec16c3a30d9297c3f511644798593
sanjuray <sanjugarimella@gmail.com>
2024-02-24 23:02:18 +0530

    initial

---
+++ app/src/main/java/com/example/navigationframework/FirstFragment.java
@@ -0,0 +1,38 @@
+package com.example.navigationframework;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(
+                R.layout.fragment_first,
+                container,
+                false);
+
+        Button btn1 = view.findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Navigation Controller
+                Navigation.findNavController(v).navigate(R.id.action_firstFragment_to_secondFragment);
+            }
+        });
+        return view;
+    }
+}
